aae28613ea38e84ce16fd627ace4f64639405062
Croz <80237957+Crozbar@users.noreply.github.com>
2023-08-30 23:51:17 -0400

    Add files via upload
    funny function

---
+++ new-lecteur.psm1
@@ -0,0 +1,15 @@
+<# Cette fonction humoristique détermine une lettre au hasard
+et crée un lecteur réseau à partir d'une destination recueillie comme argument
+#>
+function new-lecteur {
+	<#
+	.EXAMPLE
+	
+	new-lecteur \\DC01\users\michelbouchel$
+	#>
+param([string]$chemin)
+$lettre=(65..90) | Get-Random -Count 1 | % {[char]$_}
+New-PSDrive -Root "$chemin" -Name "$lettre" -PSProvider "filesystem" -Persist
+}
+
+export-modulemember -Function 'new-lecteur'
